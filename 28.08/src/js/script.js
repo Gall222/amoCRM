950f2548dbff3dad1f2f2299a9ddf3e2fa7eeab7
Sergey Galtsov <39533490+Gall222@users.noreply.github.com>
2019-08-28 21:01:30 +0300

    Add files via upload

---
+++ 28.08/src/js/script.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import '../css/style.css';
+import './script2.js';
+import iHoroscopej from "./script2";
+
+$('.button').click(function () {
+    iHoroscopej();
+});
+
+
+
+
+
+
+
+
+
+
